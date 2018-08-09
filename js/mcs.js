be1a0026e4341433b644a37276d7c2d34d6d3e02
Eriq Augustine <eriq.augustine@gmail.com>
2018-08-08 18:40:12 -0700

    Initial commit.

---
+++ js/mcs.js
@@ -0,0 +1,20 @@
+function addBox() {
+   // TODO(eriq)
+   console.log("Add Box");
+}
+
+function load() {
+   // TODO(eriq)
+   console.log("Load");
+}
+
+function download() {
+   // TODO(eriq)
+   console.log("Download");
+}
+
+function viewMode() {
+   // TODO(eriq)
+   console.log("View Mode");
+}
+
